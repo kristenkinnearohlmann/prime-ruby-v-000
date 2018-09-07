6d2ccf50698804eab928eadf62fc04b37eaa2663
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-07 16:59:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 # Add  code here!
-def prime?(integer)
-  result = ""
-  case
-    when integer <= 1
-        result = false
-      else
-        prime_check = (2..integer).to_a
-        prime_check.each do |x|
-            prime_check.delete_if { |y| ( x != y && y % x == 0 ) }
-        end
-        result = prime_check.include?(integer)
-    end
-    result
-end
+# def prime?(integer)
+#   result = ""
+#   case
+#     when integer <= 1
+#         result = false
+#       else
+#         prime_check = (2..integer).to_a
+#         prime_check.each do |x|
+#             prime_check.delete_if { |y| ( x != y && y % x == 0 ) }
+#         end
+#         result = prime_check.include?(integer)
+#     end
+#     result
+# end
